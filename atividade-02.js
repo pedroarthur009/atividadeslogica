eabf6c4242bf6a0c613e4317661b0774bc638337
Aluno SENAI <Aluno@SN-395624.edu.fiesc.com.br>
2026-03-03 20:49:30 -0300

    aprendi muito hoje

---
+++ atividade-02.js
@@ -0,0 +1,76 @@
+// ============================================================
+//   ATIVIDADE 02 – Arrays (Listas) em JavaScript
+// ============================================================
+// Instruções: resolva cada exercício no espaço indicado.
+// Use console.log() para exibir os resultados.
+// ============================================================
+
+
+// ------------------------------------------------------------
+// EXERCÍCIO 1 – Criando e acessando arrays
+// ------------------------------------------------------------
+// a) Crie uma array chamada "cores" com pelo menos 4 cores.
+// b) Exiba a primeira e a última cor usando índices.
+
+// → Seu código aqui:
+//let cores = ['azul', 'amarelo', 'vermelho', 'verde']
+
+//console.log(cores [0]);
+//console.log(cores [3])
+
+// ------------------------------------------------------------
+// EXERCÍCIO 2 – Métodos básicos
+// ------------------------------------------------------------
+// a) Adicione uma nova cor ao final de "cores", utilizando as funções de array.
+// b) Remova a primeira cor, utilizando as funções de array.
+// c) Exiba o array resultante e depois a quantidade de itens armazendos (length).
+
+// → Seu código aqui:
+let cores = ['azul', 'amarelo', 'vermelho', 'verde', 'roxo']
+cores.push('roxo')
+cores.shift()
+console.log(cores.length);
+
+
+// ------------------------------------------------------------
+// EXERCÍCIO 3 – Números
+// ------------------------------------------------------------
+// a) Crie um array de números.
+// b) Em uma única linha, exiba os valores armazenados nesta lista e a quantidade de números armazenados (quantidade de itens, não a soma)
+
+// → Seu código aqui:
+const numeross = [1, 2, 3, 4]
+
+console.log(numeross);
+console.log(numeross.length)
+
+// ------------------------------------------------------------
+// EXERCÍCIO 4 – Todos os tipos
+// ------------------------------------------------------------
+// a) Crie um array com todos os tipos de dados que você conhece (number, string...).
+// b) Em uma única linha, exiba os valores armazenados nesta lista e a quantidade de itens armazenados
+
+// → Seu código aqui:
+
+const misto = ['texto', 10, false, 'indefinido', 'nulo']
+
+console.log(misto, misto.length);
+
+
+// ------------------------------------------------------------
+// EXERCÍCIO 5 – Mudando a lista
+// ------------------------------------------------------------
+// a) Crie um array com vários dados de vários tipos diferentes.
+// b) Exiba a lista.
+// c) Remova o primeiro e o último item da lista, utilizando as funções de array.
+// d) Exiba a lista novamente.
+// e) Adicione um novo item no final e ou no início da lista.
+// f) Exiba a lista final.
+
+// → Seu código aqui:
+let nadave = ['puler', 87, false,null]
+nadave.pop()
+nadave.shift()
+console.log(nadave);
+nadave.push('jacare')
+console.log(nadave)
