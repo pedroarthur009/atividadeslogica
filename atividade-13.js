66406873a01b26f7bcfde52547dfd0b07d38b2a2
PEDRO ARTHUR FIGUR GUCKERT <pedro_guckert@estudante.sesisenai.org.br>
2026-05-19 21:14:00 -0300

    .

--- atividade-13.js
+++ atividade-13.js
@@ -1,7 +1,7 @@
 // ============================================================
 //   ATIVIDADE 13 – Estruturas de Dados: Matriz
 // ============================================================
-
+let LerTeclado = require('readline-sync')
 
 // ------------------------------------------------------------
 // EXERCÍCIO 1 – Lendo uma matriz
@@ -17,11 +17,11 @@
 // d) Exiba o canto inferior direito (use .length).
 
 // → Seu código aqui:
-console.log(matriz.length)
-console.log(matriz[0])
-console.log(matriz[1][1])
+// console.log(matriz.length)
+// console.log(matriz[0])
+// console.log(matriz[1][1])
 
-console.log(matriz.length);
+// console.log(matriz.length);
 
 console.log("_______________________________");
 
@@ -40,14 +40,14 @@ console.log("_______________________________");
 // c) Calcule e exiba a SOMA de todos os elementos da matriz.
 
 // → Seu código aqui:
-let soma = 0;
-for (let i = 0; i < mtri.length; i++) {
-  for (let j = 0; j < mtri[i].length; j++) {
-    soma += mtri[i][j];
-  }
-}
+// let soma = 0;
+// for (let i = 0; i < mtri.length; i++) {
+//   for (let j = 0; j < mtri[i].length; j++) {
+//     soma += mtri[i][j];
+//   }
+// }
 
-console.log(`${soma}`);
+// console.log(`${soma}`);
 
 
 
@@ -58,44 +58,44 @@ console.log("_______________________________");
 // EXERCÍCIO 3 – Maior e menor valor
 // ------------------------------------------------------------
 // a) Utilizando a matriz:
-   const m = [
-     [12,  7, 25],
-     [ 3, 18,  9],
-     [31, 14, 22],
-   ];
+  //  const m = [
+  //    [12,  7, 25],
+  //    [ 3, 18,  9],
+  //    [31, 14, 22],
+  //  ];
 // b) Usando for aninhado, encontre o MAIOR e o MENOR valor da matriz.
 // c) Exiba também a posição (linha, coluna) onde cada um está.
 
 // → Seu código aqui:
-let maior = m[0][0]
-let menor = m[0][0]
+// let maior = m[0][0]
+// let menor = m[0][0]
 
-let posMaior = [0, 0]
-let posMenor = [0, 0]
+// let posMaior = [0, 0]
+// let posMenor = [0, 0]
 
-for (let i = 0; i < m.length; i++) {
-  for (let j = 0; j < m[i].length; j++) {
+// for (let i = 0; i < m.length; i++) {
+//   for (let j = 0; j < m[i].length; j++) {
 
-    if (m[i][j] > maior) {
-      maior = m[i][j]
-      posMaior = [i, j]
-    }
+//     if (m[i][j] > maior) {
+//       maior = m[i][j]
+//       posMaior = [i, j]
+//     }
 
-    if (m[i][j] < menor) {
-      menor = m[i][j]
-      posMenor = [i, j]
-    }
-  }
-}
+//     if (m[i][j] < menor) {
+//       menor = m[i][j]
+//       posMenor = [i, j]
+//     }
+//   }
+// }
 
-console.log("Maior valor:", maior)
-console.log("Posição do maior:", posMaior)
+// console.log("Maior valor:", maior)
+// console.log("Posição do maior:", posMaior)
 
-console.log("Menor valor:", menor)
-console.log("Posição do menor:", posMenor)
+// console.log("Menor valor:", menor)
+// console.log("Posição do menor:", posMenor)
 
 
-console.log("_______________________________");
+// console.log("_______________________________");
 
 
 // ------------------------------------------------------------
@@ -107,9 +107,44 @@ console.log("_______________________________");
 // d) Exiba a soma da DIAGONAL principal (m[i][i]).
 
 // → Seu código aqui:
+// const matriz = [
+//   [12,  7, 25],
+//   [ 3, 18,  9],
+//   [31, 14, 22],
+// ];
+// console.log("Matriz:");
+// console.table(matriz);
 
 
-console.log("_______________________________");
+// for (let i = 0; i < matriz.length; i++) {
+//   let somaLinha = 0;
+
+//   for (let j = 0; j < matriz[i].length; j++) {
+//     somaLinha += matriz[i][j];
+//   }
+
+//   console.log(`Linha ${i}: ${somaLinha}`);
+// }
+
+
+// for (let j = 0; j < matriz[0].length; j++) {
+//   let somaColuna = 0;
+
+//   for (let i = 0; i < matriz.length; i++) {
+//     somaColuna += matriz[i][j];
+//   }
+
+//   console.log(`Coluna ${j}: ${somaColuna}`);
+// }
+
+
+// let somaDiagonal = 0;
+
+// for (let i = 0; i < matriz.length; i++) {
+//   somaDiagonal += matriz[i][i];
+// }
+
+// console.log(`Diagonal principal: ${somaDiagonal}`);
 
 
 // ------------------------------------------------------------
@@ -121,6 +156,23 @@ console.log("_______________________________");
 // c) Exiba a matriz final com console.table().
 
 // → Seu código aqui:
+let linha = LerTeclado.questionInt('Quantas linha vai ter? ')
+let coluna = LerTeclado.questionInt('Vai ter quantas colunas? ')
+
+let matriz = [];
 
 
-console.log("_______________________________");
+
+for (let i = 0; i < linha; i++) {
+  matriz[i] = [];
+
+
+for(let j = 0; j < coluna; j++) {
+  matriz[i][j] = LerTeclado.questionInt(`Digite m[${i}][${j}]:`)
+}
+}
+
+
+
+
+console.table(matriz);
